1dd6fc56c0d75f19351df67a76cf2046fb5b1bd1
Caio Miritz Raphaelli <117693498+caiomiritz@users.noreply.github.com>
2023-03-29 18:12:11 -0300

    Add files via upload

---
+++ Começando_com_a_linguagem/Aula03.py
@@ -0,0 +1,22 @@
+#-------------------ELIF---------------------------
+
+elif é outro condicional igual ao else if, para tirar os if aninhados
+esse aceita condição, diferente do else que é apenas a sobra
+
+#--------------------WHILE-------------------------
+
+while executa uma condição enquanto ela é verdadeira, assim como o if, else e elif, depois do parametro de condição
+ele tem o ":" que representa o bloco depois e tambem necessita mais de 4 espaços nesse bloco
+
+então tudo que tem dentro, precisa estar 4 espaços para frente
+
+#-------------FORMATAÇÃO DE STRINGS-----------------
+
+Dentro do print, quando queremos substutuir algo na frase por alguma variavel, podemos colocar chaves indicando onde vai ser a
+substituição.
+
+print("Rodada {} de {}".format(rodada, tentativas)) -> ali, vai ser substituido pelos valores das variáveis, usando a função format
+essa função entra como parametros os valores que queremos substituir dentro da string
+
+#--------------FOR----------------------------------
+
