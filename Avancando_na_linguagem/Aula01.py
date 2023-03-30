bc9378ecc49d4787559b19c90df9a4f96de9eacf
Caio Miritz Raphaelli <117693498+caiomiritz@users.noreply.github.com>
2023-03-30 14:13:32 -0300

    Add files via upload

---
+++ Avancando_na_linguagem/Aula01.py
@@ -0,0 +1,13 @@
+#---------------BOOLEAN-----------------
+
+No nosso exemplo, temos a criação de um loop para o jogo
+enquanto nosso carinha nao acertar e nao morrer o jogo continua, tudo isso utilizando um while
+
+podemos notar que as variaveis que vao representar nosso booleano começam com letra maiuscula False ou True
+enfocardo = False
+acertou = False
+
+palavras chave dentro do while: not e and, fazendo a função de negar a variavel junto com a necessidade dos 2 serem True
+para continuar no loop
+
+#-----------------
