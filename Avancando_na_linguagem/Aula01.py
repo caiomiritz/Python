85840848e85f282d54b2c2e11af87ea547a764a5
Caio Miritz Raphaelli <117693498+caiomiritz@users.noreply.github.com>
2023-04-04 09:24:46 -0300

    Update Aula01.py

--- Avancando_na_linguagem/Aula01.py
+++ Avancando_na_linguagem/Aula01.py
@@ -10,4 +10,13 @@ acertou = False
 palavras chave dentro do while: not e and, fazendo a função de negar a variavel junto com a necessidade dos 2 serem True
 para continuar no loop
 
-#-----------------
+#-----------------LISTA------------------
+
+A lista é uma ferramenta build-in, a mesma pode ter vários tipos de métodos que podem nos auxiliar
+para usar uma lista, basta criar uma variável e colocar []. ex: lista = []
+  
+a lista, pode ter valores pré definidos dentro dela, separada por vírgula
+
+podemos acessar o índice dela para fazer a mudança. ex: lista[0] = valor
+nesse caso, o valor do indice 0 pode ser mudado
+
