f36cfaaa0e9d58701b38243d628a4d4dc2b73223
Caio Miritz Raphaelli <117693498+caiomiritz@users.noreply.github.com>
2023-04-19 15:15:43 -0300

    Create Aula04.py

---
+++ Avancando_na_linguagem/Aula04.py
@@ -0,0 +1,17 @@
+-------------------------------FUNÇÕES---------------------------------
+
+Devemos criar funções para separar a reponsabilidade de cada coisa no código pelo 'def'
+ex: def imprime_mensagem():
+    
+Claro, quando queremos usar essa função, basta chamá-la quando quizermos, claro, se a função retorna algo
+preisamos armazenar em alguma variável ou fazer outro processo com o uso adequado
+
+Para deixar explícito que queremos retornar algo nessa função, usamos o 'return' seguido do nosso retorno
+As variáveis que estão no escopo das funções só podem ser usadas dentro da propria função
+
+Temos parâmetros das funções, que recebem informações a qual vamos utilizar na sua execução
+ex: def inicializa_letras(palavra), onde 'palavra' é um parâmetro
+Podemos colocar mais de um parâmetro também
+
+Diferente das outras linguagens, não precisamos colocar o tipo do parâmetro
+
