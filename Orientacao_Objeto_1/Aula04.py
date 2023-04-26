fc15cc1c16e1117df9bb54c33c2904f130521a45
Caio Miritz Raphaelli <117693498+caiomiritz@users.noreply.github.com>
2023-04-26 15:58:14 -0300

    Update Aula04.py

--- Orientacao_Objeto_1/Aula04.py
+++ Orientacao_Objeto_1/Aula04.py
@@ -1,2 +1,13 @@
 ------------------ METODOS PRIVADOS / ESTATICOS ----------------------
 
+Quando criamos métodos apenas para melhorar a compreensão e não queremos usar ele diretamente
+deixamos o método disponível para ser usado apenas dentro da classe, geralmente eles se encontram 
+dentro de outros métodos para melhorar o código
+
+Como as variáveis privadas, mudamos o seu nome com '__' também
+Ex: def __pode_sacar(self, parametroX)
+  
+Dentro da classe conseguimos usar, mas fora dela não, servindo apenas para auxiliar em outros métodos
+Seprar partes para o código não ficar "macarrão"
+
+
