e6853dddbc33233b4c8b782d609d3194de6261ab
Caio Miritz Raphaelli <117693498+caiomiritz@users.noreply.github.com>
2023-05-03 10:30:19 -0300

    Update Aula04.py

--- Orientacao_Objeto_1/Aula04.py
+++ Orientacao_Objeto_1/Aula04.py
@@ -10,4 +10,15 @@ Ex: def __pode_sacar(self, parametroX)
 Dentro da classe conseguimos usar, mas fora dela não, servindo apenas para auxiliar em outros métodos
 Seprar partes para o código não ficar "macarrão"
 
+Metodos estáticos são da classe, não precisamos de um objeto, como o @property temos uma sintaxe
+Ex: @staticmethod
+    def metodo():
+        ...
+  
+Dessa maneira, temos um método de classe, útil quando algo for fixo para todos os objetos
+Temos que ser cautelozos usando esses métodos, em apenas alguns casos fazem sentido
+Geralmente quando todos os objetos compartilham de uma mesma coisa em comum
+
+
+
 
