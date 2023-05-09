1ed814790919dab007410b3c7a56c07e1252d8d1
Caio Miritz Raphaelli <117693498+caiomiritz@users.noreply.github.com>
2023-05-09 11:26:22 -0300

    Update Aula01.py

--- Orientacao_Objeto_2/Aula01.py
+++ Orientacao_Objeto_2/Aula01.py
@@ -1,1 +1,22 @@
-grger
+-----------------------HERANÇA-------------------------
+
+Diminuir a duplicaçaõ de código e ter mais reuso do código
+Ligação que classes vão ter de uma classe mãe, com características compartilhadas com outras classes
+
+Ex: Temos uma classe mãe chamada Programa
+  
+Com isso, temos 2 outras classes que compartilham características de programas
+contudo, essas 2 classes tem características próprias também
+
+na criação da classe, temos que dizer qual é a classe mãe. Ex: class Filme(Programa):
+nesse caso, a classe mãe é Programa
+
+como temos o privado na classe mãe, temos que arrumar as filhas nas suas construtoras
+Contudo, podemos deixar protected, onde somente as classes filhas podem acessar
+
+Na contrutora da mãe, deixamos apenas um '_'. ex: self._nome
+Nesse caso, o atributo comum é nome, e com apenas um '_' temos esse atributo protected
+
+------------------REUTILIZAÇÃO------------------------
+
+
