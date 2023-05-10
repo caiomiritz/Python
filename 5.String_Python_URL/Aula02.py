598bc2e8bdd9371f246c56b5368cc062f7b98be0
Caio Miritz Raphaelli <117693498+caiomiritz@users.noreply.github.com>
2023-05-10 15:30:37 -0300

    Update Aula02.py

--- 5.String_Python_URL/Aula02.py
+++ 5.String_Python_URL/Aula02.py
@@ -10,5 +10,8 @@ ex: texto[0:] -> nesse caso, ela pega do incio até o final da String, claro, is
 find também pode achar uma string completa find('Caio') retornando a prosição da primeira evidencia 
 Podemos usar o len() de alguma string específica para podermos trabalhar junto com o find. Ex: len('Caio')
   
+find() também tem parametros não obrigatorios, onde tu pode colocar o inicio e o fim da busca
+find('&', 10) -> ele vai começar a buscar '&' pela posição 10 para frente
 
-
+Dessa maneira, podemos fatiar a URL com inúmeros parâmetros, usando condicionais para fazer a busca
+Assim, independente da ordem, podemos pegar cara um dos parametros da URL
