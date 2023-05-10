d72935a9f77349bfc67514daf13fb4418034685b
Caio Miritz Raphaelli <117693498+caiomiritz@users.noreply.github.com>
2023-05-10 14:49:50 -0300

    Update Aula01.py

--- 5.String_Python_URL/Aula01.py
+++ 5.String_Python_URL/Aula01.py
@@ -1,1 +1,16 @@
-Tao 
+---------------------------URLS-------------------------
+
+Iremos analizar a fundo a classe String, seus métodos e manipulação dos mesmos
+Parametros da URL, "variaveis que são passadas para o programa" -> querrys: 'q' consulta que eu fiz
+'&' separa os parametros da URL
+  
+Site é basicamente composto pelo domínio (base), seguido dos parametros que funcionam como variáveis
+
+------------------FATIAMENTO DE STRINGS---------------------
+
+O que é fatiamento ? Funcionalidade do Python, onde conseguimos extrair um pedaço de uma string
+Como uma string é tipo um array, onde para fatiar, precisamos escolher o início ( inclusivo ) e o final ( excluivo )
+
+Ex: texto = 'abcd'
+texto[0:2] -> fatiamos iniciando pelo 'a' e terminando no 'b' ( lembrando que o final é exclusivo ) ou seja -> 'ab'
+
