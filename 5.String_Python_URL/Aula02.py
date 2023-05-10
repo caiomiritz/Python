729084b00e89c20bb18e3b380d1371aa648a762a
Caio Miritz Raphaelli <117693498+caiomiritz@users.noreply.github.com>
2023-05-10 15:17:43 -0300

    Update Aula02.py

--- 5.String_Python_URL/Aula02.py
+++ 5.String_Python_URL/Aula02.py
@@ -1,2 +1,14 @@
 ------------------UTILIZANDO METODOS--------------------------
 
+Como encontrar a posição de uma string ? Usando o método find()
+ex: texto = 'abcd' -> aplicando texto.find('b') retornará a posição da letra, nesse caso 1
+
+Podemos usar isto na hora de fatiar as strings. Vale lembrar que podemos omitir um dos parametros do fatiamento
+ex: texto[:5] -> nesse caso ela pega o inicio da URL e vai até a posição 4, ja que é exclusivo o final
+ex: texto[0:] -> nesse caso, ela pega do incio até o final da String, claro, isso tudo pode ser ajustado
+  
+find também pode achar uma string completa find('Caio') retornando a prosição da primeira evidencia 
+Podemos usar o len() de alguma string específica para podermos trabalhar junto com o find. Ex: len('Caio')
+  
+
+
