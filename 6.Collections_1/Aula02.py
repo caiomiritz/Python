8eb6bda7e93c2bef2ab9ce36c177c3a7145f2fee
Caio Miritz Raphaelli <117693498+caiomiritz@users.noreply.github.com>
2023-05-16 14:52:06 -0300

    Update Aula02.py

--- 6.Collections_1/Aula02.py
+++ 6.Collections_1/Aula02.py
@@ -1,4 +1,4 @@
-------------------LISTAS DE OBJETOS----------------
+------------------LISTAS DE OBJETOS---------------------
 
 Podemos fazer listas e passar objetos instanciados para a mesma, contudo vale lembrar que
 apenas criamos uma variável que faz referencia para a o local de memória onde este objeto foi 
@@ -10,6 +10,24 @@ de acesso a esse objeto, métodos e atributos
 
 ----------------------------TUPLAS----------------------
 
+Quando queremos trabalhar com sequencias diferentes, onde cada posição representa algo diferente
+usamos as tuplas, ja que a representação é imutável
 
+Diferente da lista, as tuplas são imutáveis, fixas, determinadas apenas quando inicializamos
+uma tupla não tem método append() por exemplo, por que não podemos adicionar, remover valores dentro dela
 
+Quando temos muitos valores e funções diferentes na tupla, as classes são muito mais recomendadas
+Podemos acessar os valores, mas não podemos altera-la
 
+Unica coisa que podemos fazer é criar métodos que retornam uma tupla usando os valores de outras tuplas
+Assim podemos reatribuir esse retorno na tupla existente
+
+--------------TUPLAS + LISTAS /  OBJETO----------------
+
+Podemos ter uma lista de tuplas, onde a lista é mutável, podendo adicionar mais tuplas, ou deleta-las da lista
+para acessar uma lista de tuplas, é tipo uma matriz. Ex: usuarios[0][1] -> retorno da primeira tupla, com seu segundo valor
+  
+Assim, podemos ter tuplas de listas também, e tuplas de objetos, onde podemos alterar os objetos, pois ele é mutável
+a tupla em si não pode ser mudada
+
+Não é muito comum usar tuplas nesses cenários
