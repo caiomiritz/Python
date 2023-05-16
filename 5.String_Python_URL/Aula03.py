765a841c15f34ac1c6d2292f2054c90bfed12367
Caio Miritz Raphaelli <117693498+caiomiritz@users.noreply.github.com>
2023-05-16 11:23:42 -0300

    Update Aula03.py

--- 5.String_Python_URL/Aula03.py
+++ 5.String_Python_URL/Aula03.py
@@ -19,3 +19,26 @@ None não tem o método Strip da String
 String vazia (empty) e None são diferentes, mas ambos são considerados false
 É bom ter um if para verificar se o objeto é um None ou é String vazia
 
+---------------------EXPRESSÕES REGULARES-----------------
+
+import re -> "Regular Expressions" / "ReqEx"
+é biblioteca padrão do Pyhton
+
+padrao = re.compile("[]") -> Dentro dos [] mostramos o padrão da escrita, retornando o objeto padrão
+pódemos colocar "?" para mostra se o grupo dentro do [] pode aparecer ou não
+o obejto padrao tem métodos, como search() retornando #match ou none, caso o padrão não seja achado
+
+busca = padrao.search(String qualquer)
+if busca:
+  cep = busca.group()  -> retorna a string encontrada com aquele padrão
+  print(cep)
+
+----------------------QUANTIFICADORES E INTERVALOS--------------
+
+Podemos passar um número dentro {} para mostra quantas vezes aquele grupo [] aparece no padrão, reduzindo muito o compile()
+Ex: busca de cep: padrao = re.compile("[0-9]{5}[-]?[0-9]{3}") -> um cep composto de 5 dígitos de 0 a 9 seguido de um hífen opcional
+mais 3 dígitos de 0 a 9 também
+
+{} -> nos quantificadores podemos mostrar quantas vezes ele pode aparecer {0,1} -> pode aparecer 0 ou 1 vez o item no padrão
+
+
