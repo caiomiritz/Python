89fe9fb63374b4b1aded70aeba1712d8be7176f1
Caio Miritz Raphaelli <117693498+caiomiritz@users.noreply.github.com>
2023-05-16 14:17:40 -0300

    Create Aula02.py

---
+++ 6.Collections_1/Aula02.py
@@ -0,0 +1,15 @@
+------------------LISTAS DE OBJETOS----------------
+
+Podemos fazer listas e passar objetos instanciados para a mesma, contudo vale lembrar que
+apenas criamos uma variável que faz referencia para a o local de memória onde este objeto foi 
+instanciado, nós não instanciamos objetos novos, quando passamos eles por parâmetro
+
+Assim, podemos mudar esse objeto, usando outras variáveis, ou seja, um mesmo objeto pode ter
+mais de uma variável que referencia-o, o que pode causar problemas devido a mutabilidade e a facilidade
+de acesso a esse objeto, métodos e atributos
+
+----------------------------TUPLAS----------------------
+
+
+
+
