d79d67563790a969aec7dbe7f420e235cf850295
Caio Miritz Raphaelli <117693498+caiomiritz@users.noreply.github.com>
2023-05-17 14:32:30 -0300

    Update Aula01.py

--- 7.Collections_2/Aula01.py
+++ 7.Collections_2/Aula01.py
@@ -1,12 +1,17 @@
-endereco = "Rua da Flores 72, apartamento 1002, Laranjeiras, Rio de Janeiro, RJ, 23440-120"
+---------------------CONJUNTOS 'set'-------------------------------
 
+Além das listas, tuplas, temos outras maneiras de fazer conjuntos de valores, objetos, etc
+Ex: temos situações que queremos um conjunto de elementos sem repetição, usamos o 'set'
 
-import re  # Regular Expression -- RegEx
+set(listaX) -> nessa situação, transformamos uma lista em um set, nesse caso ele tira os elementos repetidos
+para criar um conjunto set, usamos {}, onde o acesso sequencial não é importante, a ordem não importa
 
-# 5 dígitos + hífen (opcional) + 3 dígitos
+uasuarios = {1, 6, 33, 3} -> nesse caso ele fica como {1, 3, 6, 33}
 
-padrao = re.compile("[0-9]{5}[-]{0,1}[0-9]{3}")
-busca = padrao.search(endereco)  # Match
-if busca:
-    cep = busca.group()
-    print(cep)
+Como não existe posição no conjunto 'set' não conseguimos acessar o "indice" pois não tem ordem, apenas os elementos
+assim, nao conseguimos pergar um elemento específico, mas podemos iterar no conjunto, pois ele é um iterável
+
+A união de conjuntos, temos que usar a operação 'ou' -> '|' ex: asuarios | contas -> dessa maneira temos a união deles
+a operação '&' ele aparece os elementos que são iguais dos conjuntos
+a operação '-' mostra apenas os elementos de um conjunto que não esta no outro
+assim como tudo, a documentação mostra outras operações de conjuntos que podemos fazer
