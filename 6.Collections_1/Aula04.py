37c61672bd5482f314678be9484f1fcd0e90776a
Caio Miritz Raphaelli <117693498+caiomiritz@users.noreply.github.com>
2023-05-17 13:52:00 -0300

    Update Aula04.py

--- 6.Collections_1/Aula04.py
+++ 6.Collections_1/Aula04.py
@@ -20,3 +20,35 @@ reversed(lista) -> faz a mesma coisa, mas ao contrário de sorted() ele não ret
 Claro, a prórpia lista tem métodos que fazem isso, contudo list.sort() ordena diretamente a lista, ja que é mutável
 diferente do sorted() ela não retorna nada, apenas altera no local (inplace) a lista
 
+Tudo isso segue uma ordem natural que a máquina segue, ex: Strings se diferenciam de maiúsculas e minúsculas
+
+Ordenação sem ordem natural segue uma maneira diferente, ex: uma lista de objetos do tipo "Contas" 
+a chave para fazer pode ser feita por uma key no parâmetro de sorted(). 
+
+Ex: sorted(contas, key=método):  -> nesse caso, o método retorna a compração que quizer, reduzindo o objeto a um valor comparável
+Abordagem funcional
+
+from operator import attrgetter
+Nesse caso, não precisamos criar o método na key, pois esse import consegue pegar o atributo do objeto
+
+Ex: sorted(contas, key=attrgetter("_saldo", "_codigo")):  -> nesse caso, ele pega o atributo protect _saldo como primeiro
+se empatar, usamos o atributo _codigo para desempate
+    
+Problema, como o atributo é protected, não queremos alterar ou acessar esses atributos, pois não é permitido.
+Para podemos fazer um sorted direto, ou alguma comparação que envolva '<' ou algo do tipo, podemos sobrescrever o metodo __lt__ (less than)
+nesse caso, podemos fazer a ordenação natural
+
+def __lf__(self, other): -> nessa sobrescrição ela faz a comparação que quizermos 
+  
+para desempatar, usando o __lt__ podemos complexar o código com condicionais para fazer as análises
+
+functools -> essa biblioteca da para uma classe uma série de utilidades
+from functools import total_ordering
+
+Dessa maneira, apenas precisamos implementar as funções __eq__ e __lt__ que a propria biblioteca nos da uma série decomparações prontas, usando
+métodos prontos, tendo assim uma ordenação total sobre essa comparação de objetos, '<' '>' '<=' '==' etc.
+
+@total_ordering
+class Conta():     -> temos que sinalizar que essa conta possui o aspecto de ordenação total
+
+
